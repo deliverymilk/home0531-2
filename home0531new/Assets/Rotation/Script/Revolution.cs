3f93fbad486032a391d4ee7ae026d1bb60ad22b3
aaaa1ass <133478663+deliverymilk@users.noreply.github.com>
2023-06-24 16:34:04 +0900

    2023.6.24rotation

---
+++ home0531new/Assets/Rotation/Script/Revolution.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Revolution : MonoBehaviour
+{
+    public GameObject moon;
+    public GameObject origin;
+    public float speed = 1.0f;
+    
+    private void Start()
+    {
+        StartCoroutine(RotateCoroutine());
+        
+    }
+
+    IEnumerator RotateCoroutine()
+    {
+        while(true)
+        {
+            transform.RotateAround
+            (
+                origin.transform.position,
+                Vector3.down,
+                speed * Time.deltaTime
+            );
+
+            moon.transform.Rotate(0.1f, 0.1f, 0.1f);
+
+            yield return null;
+        }
+    }
+
+
+
+}
