750fdf30c14393523a964b09bf8ec10f3a5bc1c0
aaaa1ass <133478663+deliverymilk@users.noreply.github.com>
2023-06-24 17:39:29 +0900

    다형성파일

---
+++ Consolegame/Consolegame/Consolegame.cpp
@@ -0,0 +1,66 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+class A
+{
+public :
+
+	A()
+	{
+		cout << "A 생성자" << endl;
+	}
+
+	virtual void VirtualFunction()
+	{
+		cout << "A 클래스의 가상 함수" << endl;
+	}
+
+	void Function()
+	{
+		cout << "A 클래스의 함수" << endl;
+	}
+
+	~A()
+	{
+		cout << "A 소멸자" << endl;
+	}
+};
+
+
+class B : public A
+{
+public:
+	B()
+	{
+		cout << "B 생성자" << endl;
+	}
+
+	virtual void VirtualFunction()
+	{
+		cout << "B 클래스의 가상 함수" << endl;
+	}
+	void Function()
+	{
+		cout << "B 클래스의 함수" << endl;
+	}
+
+	~B()
+	{
+		cout << "B 소멸자" << endl;
+	}
+
+};
+
+int main()
+{
+	//B b;
+	//b.Function();
+
+	A* ptr = new B;
+
+	ptr->VirtualFunction();
+	ptr->Function();
+}
+
